46994b09d2b86faceef9ebaa3dcc586ff77854a9
Norman Markgraf <nmarkgraf@hotmail.com>
2020-06-09 11:10:50 +0200

    Kleines Update für die Erstellung der prelude.csv!

--- bin/CreateNewPreludeCsv.sh
+++ bin/CreateNewPreludeCsv.sh
@@ -7,6 +7,14 @@ grep -o "library(.*)" Inhalte/*.Rmd | \
          print "\"tinytex\", \"0.4\""
          print "\"knitr\", \"1.20\""
          print "\"futile.logger\", \"0\""
-    } {print "\"" $2 "\", \"0\""}' |\
+    } {
+        x = $2
+        if ($2 == "library") {
+            x = $3
+        }
+        if (x != "XXX") {
+            print "\"" x "\", \"0\""
+        }
+      }' |\
     awk 'BEGIN{print "packages, minrelease"} {print $0} ' |\
     sort -r | awk '!a[$1]++' | sort -r
